efab0e5d446078c9eacab4e6fde684aad5386e62
wazzuck <nevillebryce@gmail.com>
2026-03-27 11:27:09 +0000

    Updating

--- header-loader.js
+++ header-loader.js
@@ -8,14 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const homeLink = isServicePage ? '../index.html' : 'index.html';
         const logoImg = isServicePage ? '../assets/glasses.png' : '/assets/glasses.png';
         
-        // Use the exact homepage header format
+        // Use the exact homepage header format - logo only
         headerElement.innerHTML = `
             <a href="${homeLink}" class="logo"><img src="${logoImg}" alt="IToldYouSo" /> <span><i>IT</i>oldYouSo Computing</span></a>
-            <div class="phone-info">
-                <a href="tel:07809590652" class="phone-number">07809590652</a>
-                <a href="mailto:contactus@itoldyouso.tech" class="email-address">contactus@itoldyouso.tech</a>
-                <p class="consultation-text">Free consultation</p>
-            </div>
         `;
     }
 });
